e4e556b99f46bea8471b8c424450017b74cc033a
MalsR <malinda_rajapakse@hotmail.com>
2016-05-25 08:12:27 +0100

    Updated server with multiple clients example

--- src/main/java/malsr/basicserver/ServerMultipleClients.java
+++ src/main/java/malsr/basicserver/ServerMultipleClients.java
@@ -7,38 +7,45 @@ import java.net.SocketAddress;
 
 public class ServerMultipleClients {
 
-    public static void main(String[] args) {
-            try (final ServerSocket serverSocket = new ServerSocket(2003)) {
-
-                int count = 0;
-                while (true) {
-                    System.out.println("Creating thread =" + count++);
+    public static void main(String[] args) throws IOException {
+        boolean listenForClientConnection = true;
+        try (final ServerSocket serverSocket = new ServerSocket(2003)) {
+                int count = 1;
+                while (listenForClientConnection) {
 
                     final Socket clientConnection = serverSocket.accept();
-
-                    new Thread() {
-
-                        @Override
-                        public void run() {
-                            System.out.println("Executing run method");
-                            try (InputStream clientInputStream = clientConnection.getInputStream();
-                                 BufferedReader clientBufferedReader = new BufferedReader(new InputStreamReader(clientInputStream));) {
-
-                                System.out.println("Got client connection");
-                                String clientInput;
-                                SocketAddress remoteSocketAddress = clientConnection.getRemoteSocketAddress();
-
-                                while ((clientInput = clientBufferedReader.readLine()) != null) {
-                                    System.out.println("Client " + remoteSocketAddress + " said = " + clientInput);
-                                }
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }.start();
+                    System.out.println("Creating thread =" + count++);
+                    
+                    handleClientRequest(clientConnection);
                 }
             } catch (IOException e) {
-                e.printStackTrace();
+                throw e;
             }
+    }
+
+    private static void handleClientRequest(final Socket clientConnection) {
+        Thread clientRequestThread = new Thread() {
+
+            @Override
+            public void run() {
+                System.out.println("Executing run method");
+                try (InputStream clientInputStream = clientConnection.getInputStream();
+                     InputStreamReader inputStreamReader = new InputStreamReader(clientInputStream);
+                     BufferedReader clientBufferedReader = new BufferedReader(inputStreamReader)) {
+
+                    System.out.println("Got client connection");
+                    String clientInput;
+                    SocketAddress remoteSocketAddress = clientConnection.getRemoteSocketAddress();
+
+                    while ((clientInput = clientBufferedReader.readLine()) != null) {
+                        System.out.println("Client " + remoteSocketAddress + " said = " + clientInput);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        clientRequestThread.start();
     }
 }
