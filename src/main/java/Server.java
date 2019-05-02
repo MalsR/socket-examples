a387b769551d978e86715bf35de637d07a7776fd
Malinda Rajapakse <malinda_rajapakse@hotmail.com>
2019-05-02 09:47:46 +0100

    A Test commit to test waffle alternatives
    As part of https://app.clubhouse.io/testcoreteam/story/12/example-issue-to-deliver-epic [ch12]

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -9,7 +9,7 @@ public class Server {
 
     private static final int portNumber = 6660;
 
-//Test commit
+//Test commit again
     public static void main(String[] args) throws InterruptedException, IOException {
 
         try {
